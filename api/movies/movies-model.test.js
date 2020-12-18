f7e2faef4690bed590c1152a14f3498afe92e6b0
LBU <lbu@LBUui-MacBook-Pro.local>
2020-12-17 20:20:17 -0500

    mvp met

--- api/movies/movies-model.test.js
+++ api/movies/movies-model.test.js
@@ -27,6 +27,29 @@ describe('movies model', () => {
             await Movies.insert({ name: 'lord of the rings' })
             let movies = await db('movies')
             expect(movies).toHaveLength(1)
+
+            await Movies.insert({ name: 'dumb and dumber' })
+            movies = await db('movies')
+            expect(movies).toHaveLength(2)
+        })
+        it('can give back the inserted movies', async () => {
+            const lotr = await Movies.insert({ name: 'lord of the rings' })
+            expect(lotr).toMatchObject({ id: 1, name: 'lord of the rings'})
+        })
+    })
+    describe('update', () => {
+        it('can insert', async () => {
+            await db('movies').insert({ name: 'iron man'})
+            let im = await Movies.update(1, {name : 'iron man'})
+            expect(im).toMatchObject({ id: 1, name: 'iron man'})
+        })
+    })
+    describe('remove', () => {
+        it('can remove', async() => {
+            await db('movies').insert({ name: 'lord of the rings'})
+            await Movies.remove(1)
+            let movies = await db('movies')
+            expect(movies).toHaveLength(0)
         })
     })
 })
